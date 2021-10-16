8dd5fee06844f5188093dfa240cedb9cd0c53eb6
Yuval <yuvaluner@gmail.com>
2021-10-16 20:16:14 +0300

    Added pearson

--- SourceCode/yuval_anomaly_detection.cpp
+++ SourceCode/yuval_anomaly_detection.cpp
@@ -2,18 +2,23 @@
 #include <math.h>
 
 float var(float* x, int size){
-    float miu = getMiu(x, size);
+    float miu = getAvg(x, size);
     float var = 0;
-    for (int i = 1; i <= size; i++){
+    for (int i = 0; i < size; i++){
         var += pow((x[i] - miu), 2);
     }
     return var / size;
 }
 
-float getMiu(float* x, int size){
+float getAvg(float* x, int size){
     float miu = 0;
-    for (int i = 1; i <= size; i++){
+    for (int i = 0; i < size; i++){
         miu += x[i];
     }
     return miu / size;
 }
+
+float pearson(float* x, float* y, int size){
+    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+}
+
