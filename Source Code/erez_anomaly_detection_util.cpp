e20a61ce5b2fb4f99dd0cee535014ae2c6f0d28c
erez <erezelgrably@hotmail.com>
2021-10-16 21:24:28 +0300

    add 2 last functions

--- Source Code/erez_anomaly_detection_util.cpp
+++ Source Code/erez_anomaly_detection_util.cpp
@@ -19,8 +19,8 @@ float getAvg(float* x, int size) {
 }
 
 float cov(float* x, float* y, int size) {
-    float xAvg = getAvg(float* x, int size);
-    float yAvg = getAvg(float* y, int size);
+    float xAvg = getAvg(x, size);
+    float yAvg = getAvg(y, size);
     float getE = 0;
     for (int i = 0; i < size; i++) {
         getE += (x[i] - xAvg) * (y[i] - yAvg);
@@ -29,5 +29,27 @@ float cov(float* x, float* y, int size) {
 }
 
 float pearson(float* x, float* y, int size) {
+    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+}
 
+Line linear_reg(Point** points, int size) {
+    float xValues[size];
+    float yValues[size];
+    for (int i = 0; i < size; i++) {
+        xValues[i] = points[i]->x;
+        yValues[i] = points[i]->y;
+    }
+    float a = cov(xValues, yValues, size) / var(xValues, size);
+    float b = getAvg(yValues, size) - (a * getAvg(xValues, size));
+    return Line(a, b);
+}
+// returns the deviation between point p and the line equation of the points 
+float dev(Point p, Point** points, int size) {
+    Line line = linear_reg(points, size);
+    return float deviation = abs(line.f - p.y);
+}
+
+// returns the deviation between point p and the line 
+float dev(Point p, Line l) {
+    return abs(l.f - p.y);
 }
