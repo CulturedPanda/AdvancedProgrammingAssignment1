5311218e30cf5f60879e4e2a1bc0c395dfda516e
Yuval <yuvaluner@gmail.com>
2021-10-16 20:57:01 +0300

    Added linear_reg

--- Source Code/yuval_anomaly_detection.cpp
+++ Source Code/yuval_anomaly_detection.cpp
@@ -18,7 +18,29 @@ float getAvg(float* x, int size){
     return miu / size;
 }
 
+float cov(float* x, float* y, int size) {
+    float xAvg = getAvg(x, size);
+    float yAvg = getAvg(y, size);
+    float getE = 0;
+    for (int i = 0; i < size; i++) {
+        getE += (x[i] - xAvg) * (y[i] - yAvg);
+    }
+    return getE / size;
+}
+
 float pearson(float* x, float* y, int size){
     return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+}
+
+Line linear_reg(Point** points, int size){
+    float xValues[size];
+    float yValues[size];
+    for (int i = 0; i < size; i++){
+        xValues[i] = points[i]->x;
+        xValues[i] = points[i]->y;
+    }
+    float a = cov(xValues, yValues, size) / var(xValues, size);
+    float b = getAvg(yValues, size) - (a * getAvg(xValues, size));
+    return Line(a, b);
 }
 
