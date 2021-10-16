daaadecdec613f4b3d4bcb4455c162b3d42656f4
Yuval <yuvaluner@gmail.com>
2021-10-16 22:41:40 +0300

    Fuck my life

--- Source Code/anomaly_detection_util.cpp
+++ Source Code/anomaly_detection_util.cpp
@@ -6,7 +6,9 @@
 */
 #include "anomaly_detection_util.h"
 #include <math.h>
-#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 // Returns the mathematical average of from an array of floats
 float getAvg(float* x, int size) {
@@ -21,8 +23,8 @@ float getAvg(float* x, int size) {
 float var(float* x, int size) {
     float miu = getAvg(x, size);
     float var = 0;
-    for (int i = 1; i <= size; i++) {
-        var += pow((x[i] - miu), 2);
+    for (int i = 0; i < size; i++) {
+        var += std::pow((x[i] - miu), 2);
     }
     return var / size;
 }
@@ -40,7 +42,7 @@ float cov(float* x, float* y, int size) {
 
 // returns the Pearson correlation coefficient of X and Y 
 float pearson(float* x, float* y, int size) {
-    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+    return cov(x, y, size) / (std::sqrt(var(x, size)) * std::sqrt(var(y, size)));
 }
 
 // performs a linear regression and return s the line equation 
@@ -60,11 +62,44 @@ Line linear_reg(Point** points, int size) {
 // returns the deviation between point p and the line equation of the points 
 float dev(Point p, Point** points, int size) {
     Line line = linear_reg(points, size);
-    float deviation = abs(line.f(p.x) - p.y);
+    float deviation = std::abs(line.f(p.x) - p.y);
     return deviation;
 }
 
 // returns the deviation between point p and the line 
 float dev(Point p, Line l) {
-    return abs(l.f(p.x) - p.y);
+    return std::abs(l.f(p.x) - p.y);
+}
+
+bool wrong(float val, float expected){
+	return val<expected-0.001 || val>expected+0.001;
+}
+
+// this is a simple test to put you on the right track
+int main(){
+	const int N=10;
+	float x[]={1,2,3,4,5,6,7,8,9,10};
+	float y[]={2.1,4.2,6.1,8.1,10.3,12.2,14.4,16.1,18.2,20.3};
+
+	Point* ps[N];
+	for(int i=0;i<N;i++)
+		ps[i]=new Point(x[i],y[i]);
+
+	Line l=linear_reg(ps,N);
+	Point p(4,8);
+
+	float v[]={var(x,N),cov(x,y,N),pearson(x,y,N),l.a,l.b,l.f(4),dev(p,l)};
+	float e[]={8.25,16.63,0.999,2.015,0.113,8.176,0.176};
+
+
+	for(int i=0;i<7;i++)
+		if(wrong(v[i],e[i]))
+			cout<<"error for check "<<i<<" (-14)"<<endl;
+
+
+	for(int i=0;i<N;i++)
+		delete ps[i];
+
+	cout<<"done"<<endl;
+	return 0;
 }
