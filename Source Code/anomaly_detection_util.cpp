993af9ed342ba42069200cbb833efd77ecf85d16
Yuval <yuvaluner@gmail.com>
2021-10-21 16:10:40 +0300

    Updated to fabs and sqrtf

--- Source Code/anomaly_detection_util.cpp
+++ Source Code/anomaly_detection_util.cpp
@@ -6,17 +6,6 @@
 */
 #include "anomaly_detection_util.h"
 #include <math.h>
-#include <iostream>
-
-using namespace std;
-
-// Returns the absolute value of a float
-float absValue(float val){
-    if (val < 0){
-        return -val;
-    }
-    return val;
-}
 
 // Returns the mathematical average of from an array of floats
 float getAvg(float* x, int size) {
@@ -59,7 +48,7 @@ float cov(float* x, float* y, int size) {
 
 // returns the Pearson correlation coefficient of X and Y 
 float pearson(float* x, float* y, int size) {
-    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+    return cov(x, y, size) / (sqrtf(var(x, size)) * sqrtf(var(y, size)));
 }
 
 // performs a linear regression and return s the line equation 
@@ -79,46 +68,11 @@ Line linear_reg(Point** points, int size) {
 // returns the deviation between point p and the line equation of the points 
 float dev(Point p, Point** points, int size) {
     Line line = linear_reg(points, size);
-    float deviation = absValue(line.f(p.x) - p.y);
+    float deviation = fabs(line.f(p.x) - p.y);
     return deviation;
 }
 
 // returns the deviation between point p and the line 
 float dev(Point p, Line l) {
-    return absValue(l.f(p.x) - p.y);
-}
-
-//-----------------------------------------------------------------------
-
-bool wrong(float val, float expected){
-	return val<expected-0.001 || val>expected+0.001;
-}
-
-// this is a simple test to put you on the right track
-int main(){
-	const int N=10;
-	float x[]={1,2,3,4,5,6,7,8,9,10};
-	float y[]={2.1,4.2,6.1,8.1,10.3,12.2,14.4,16.1,18.2,20.3};
-
-	Point* ps[N];
-	for(int i=0;i<N;i++)
-		ps[i]=new Point(x[i],y[i]);
-
-	Line l=linear_reg(ps,N);
-	Point p(4,8);
-
-	float v[]={var(x,N),cov(x,y,N),pearson(x,y,N),l.a,l.b,l.f(4),dev(p,l)};
-	float e[]={8.25,16.63,0.999,2.015,0.113,8.176,0.176};
-
-
-	for(int i=0;i<7;i++)
-		if(wrong(v[i],e[i]))
-			cout<<"error for check "<<i<<" (-14)"<<endl;
-
-
-	for(int i=0;i<N;i++)
-		delete ps[i];
-
-	cout<<"done"<<endl;
-	return 0;
+    return fabs(l.f(p.x) - p.y);
 }
